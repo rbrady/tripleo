2d6aad5ac68b330e01af893f4433bcfda44a7988
Ryan Brady <ryan@ryanbrady.org>
2013-12-05 09:23:56 -0500

    changed order of activities

--- setup.sh
+++ setup.sh
@@ -5,20 +5,20 @@ set -eux
 # install prereqs
 sudo yum install -y git vim
 
+# clone undercloud-live
+git clone https://github.com/rbrady/undercloud-live
+
 # get extras
 mkdir -p ~/extra
-cd ~/extra
+pushd ~/extra
 git clone https://github.com/rbrady/tripleo-heat-templates.git
 git clone https://github.com/rbrady/flaming-octo-tyrion.git
 
 # fix sudo
-sudo flaming-octo-tyrion/fix_sudo.sh rh
+pushd flaming-octo-tyrion
+sudo ./fix_sudo.sh rh
 
 # set selinux to permissive
 sudo sed -i "s/=enforcing/=permissive/"
-
-# clone undercloud-live
-cd
-git clone https://github.com/rbrady/undercloud-live
 
 echo "The machine has been prepared to become the undercloud.  You must reboot to complete the selinux configuration changes."
