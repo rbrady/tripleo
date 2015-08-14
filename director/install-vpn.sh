1bfeb6587e43ff0e30c70ec49af4ceaec6188819
Ryan Brady <ryan@ryanbrady.org>
2015-08-14 15:21:05 -0400

    Update install-vpn.sh

--- director/install-vpn.sh
+++ director/install-vpn.sh
@@ -14,11 +14,20 @@ sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.n
 sudo yum install openvpn
 sudo yum remove epel-release
 
-# configure openvpn
+# if the archive isn't available, attempt to download it
 if [ -n "$OPENVPN_ARCHIVE" ]; then
   curl -L -O $OPENVPN_ARCHIVE
-  tar -xvf `basename $OPENVPN_ARCHIVE`
+fi
+
+# configure openvpn
+if [ -f 'openvpn.tar.gz' ]; then
+  echo "openvpn file exists"
+  tar -xvf openvpn.tar.gz
   sudo cp etc/openvpn/* /etc/openvpn/
+  # fix jacked up archive
+  curl -L -O https://raw.githubusercontent.com/rbrady/tripleo/master/director/phx2-udp.conf
+  sudo mv phx2-udp.conf /etc/openvpn/
 else
-  echo "OPENVPN_ARCHIVE not set.  Exiting."
+  echo "No openvpn.tar.gz file."
+  echo "You need to manually configure openvpn"
 fi
