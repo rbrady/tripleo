847037dae3f1f6a11e6693a19e69198bef1dd9eb
Ryan Brady <ryan@ryanbrady.org>
2015-09-03 10:27:29 -0400

    Update install-undercloud.sh

--- director/install-undercloud.sh
+++ director/install-undercloud.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "root" == $(whoami) ]; then
+  echo "cannot run script as root.  use the stack user."
+  exit 1
+fi
+
 sudo rpm -ivh http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
 sudo rhos-release 7-director
 sudo yum install -y python-rdomanager-oscplugin
