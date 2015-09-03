2d526ecf9ba247a8e805f6d672883bfaa17c34ed
Ryan Brady <ryan@ryanbrady.org>
2015-09-03 10:55:44 -0400

    Update install-undercloud.sh

--- director/install-undercloud.sh
+++ director/install-undercloud.sh
@@ -9,3 +9,8 @@ sudo rpm -ivh http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-re
 sudo rhos-release 7-director
 sudo yum install -y python-rdomanager-oscplugin
 openstack undercloud install
+
+curl https://raw.githubusercontent.com/rbrady/tripleo/master/director/get-overcloud-images.sh | bash-x
+openstack overcloud image upload
+
+curl https://raw.githubusercontent.com/openstack/tuskar-ui/master/nodes.sh | bash -x
